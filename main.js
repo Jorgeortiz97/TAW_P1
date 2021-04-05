9b202b891d21c1c140537b1ebf99ca6225c806f8
jorgeortiz97 <jorgeortiz97@um.es>
2021-04-05 22:40:48 +0200

    Inclusión de código JS para la carga de contenido en portada

--- main.js
+++ main.js
@@ -1,16 +1,75 @@
-// resize header to size of browser window
 
-var ready = (callback) => {
-	if (document.readyState != "loading") callback();
-	else document.addEventListener("DOMContentLoaded", callback);
+/* CUSTOMIZATION PARAMETERS */
+const API_BASE_URL = "https://spaceflightnewsapi.net/api/v2/articles/";
+const API_REQ_BY_PAGE = 6;
+
+							 //                \./
+const PAGINATION_OFFSET = 2; // [Prev] [1] [2] [3] [4] [5] [Next]
+							 //        < off >     < off >
+var currentPage = 0
+
+
+function generatePaginationItem(dispName, page, isActive, isEnabled) {
+	var html = '';
+
+	if (isActive)
+		html += '<li class="page-item active">';
+	else if (isEnabled)
+		html += '<li class="page-item">';
+	else
+		html += '<li class="page-item disabled">';
+
+	html += '<a class="page-link" href="javascript:void(0);" onclick="loadArticles(' + page + ');">';
+	html += dispName;
+	html += '</a></li>';
+	return html;
 }
 
-ready(() => { 
-	document.querySelector(".header").style.height = window.innerHeight + "px";
-})
+function loadArticles(page) {
+
+	var xmlhttp = new XMLHttpRequest();
+	url = API_BASE_URL + "?_limit=" + API_REQ_BY_PAGE + "&_start=" + page * API_REQ_BY_PAGE;
+	xmlhttp.open("GET", url, true);
+	xmlhttp.onreadystatechange = function() {
+		if(xmlhttp.readyState === 4 && xmlhttp.status === 200){
+			var articles = JSON.parse(xmlhttp.responseText);
+
+			var articlesHtml = '';
+			for (i = 0; i < articles.length; i++)
+			articlesHtml += '<div class="col-lg-4 col-md-6 col-sm-12">' +
+							'<h3 class="feature-title"><a href="' + articles[i].url + '">' + articles[i].title + '</a></h3>' +
+							'<img src="' + articles[i].imageUrl + '" class="img-fluid">' +
+							'<p>' + articles[i].summary + '</p>' +
+							'</div>';
+
+			document.getElementById("articles-data").innerHTML = articlesHtml;
+
+			var paginationHtml = '';
+			if (page == 0)
+				paginationHtml += generatePaginationItem("Previous", page - 1, false, false);
+			else
+				paginationHtml += generatePaginationItem("Previous", page - 1, false, true);
+
+			for (i = -PAGINATION_OFFSET; i < PAGINATION_OFFSET + 1; i++)
+				if (page + i >= 0)
+					paginationHtml += generatePaginationItem(page + i + 1, page + i, i == 0, true);
+
+			paginationHtml += generatePaginationItem("Next", page + 1, false, true);
+
+			document.getElementById("pagination-data").innerHTML = paginationHtml;
+			currentPage = page;
+		}
+	}
+	xmlhttp.send();
+}
+
+window.onload = function() {
+	loadArticles(currentPage);
+};
+
 
 // set modal time delay before loading
 
-setTimeout(function() {
-	$('#demo-modal').modal();
-}, 500);
+//setTimeout(function() {
+//	$('#demo-modal').modal();
+//}, 500);
